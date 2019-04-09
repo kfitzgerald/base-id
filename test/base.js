7da4a3e61ecd1f978ad3729206e419d0dfd31680
Kevin Fitzgerald <kevin@kevinfitzgerald.net>
2019-04-09 13:28:35 -0500

    v2.1.0 – Fixed `base.bytesToHex(bytes)` not always prefixing hex values with `0`

--- test/base.js
+++ test/base.js
@@ -12,5 +12,18 @@ describe('Base', () => {
         should(function() { b.decode('nope') }).throw(/overridden/);
     });
 
+    it('should convert hex to bytes and vise versa', () => {
+
+        const bytes = [ 0x00, 0x01, 0x00, 0xff ];
+        const base = new Base();
+
+        base.bytesToHex(bytes).should.be.exactly('000100ff');
+        const buffer = Buffer('000100ff', 'hex');
+
+        buffer.length.should.be.exactly(4);
+        Array.from(buffer).should.deepEqual(bytes);
+
+    });
+
 });
 
